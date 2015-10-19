703a5208010f1dd6397a28b8521ee20ba45d3783
David710 <bednarski_david@hotmail.com>
2015-10-19 11:39:55 +0200

    new todo form set

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,24 @@
 class TodoListsController < ApplicationController
+  def new
+  end
+  
+  def show
+    @todo_list = TodoList.find(params[:id])
+  end
+  
+  def create
+    @todo_list = TodoList.new(todo_list_params)
+
+    @todo_list.save
+    redirect_to @todo_list
+  end
+  
   def index
     @todo_lists = TodoList.all
   end
+  
+  private
+    def todo_list_params
+      params.require(:todo_list).permit(:title, :description)
+    end
 end
