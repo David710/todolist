25ae980b01194f8a51ebc682bded8874af81385f
David710 <bednarski_david@hotmail.com>
2015-11-05 23:12:24 +0100

    delete todos added

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,6 +4,13 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to todo_list_path(@todo_list)
   end
+  
+  def destroy
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.destroy
+    redirect_to todo_list_path(@todo_list)
+  end
  
   private
     def todo_item_params
