13faa6291c0d7c7511734333091051db5624469b
David710 <bednarski_david@hotmail.com>
2016-03-29 22:46:59 +0200

    CORS implemente

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'todo_lists#index'
-  
+
+  match 'todo_items', to: 'todo_items#new', via: [:options]
+
+
   resources :todo_lists do
     member do
       post 'sort'
